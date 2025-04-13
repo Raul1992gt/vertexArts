4b3012097f341369b4eece33ef4f3bcb81815a68
Raul <rgarciat94@gmai.com>
2025-04-13 12:00:57 +0200

    Añadir 'production' al next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production'; // Usa `===` en vez de `=`
+
+const nextConfig = {
+    basePath: isProd ? '/vertexArts' : '',
+}
 
 export default nextConfig;
